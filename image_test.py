c1b9e39b8a5007b2b9dd2d43dab51b8ac36d4dd7
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-06-18 22:27:55 -0700

    remove timeout from img POST test

--- image_test.py
+++ image_test.py
@@ -2,14 +2,11 @@ import requests
 import os
 
 cwd = os.getcwd()
-image = cwd + '/assets/images/zelda02.bmp'
+image = cwd + "/assets/images/zelda02.bmp"
 
 with open(image, "rb") as f:
     img_data = f.read()
 
 files = {"image": ("zelda02.bmp", img_data)}
-    
 test_url = "http://192.168.0.213/test_image"
-r = requests.post(test_url, files=files ,timeout=60)
-
-
+r = requests.post(test_url, files=files)
