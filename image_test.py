ba1991461735d3339ad0865dbbd64b8841df82d4
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-06-19 16:07:51 -0700

    update POST image test

--- image_test.py
+++ image_test.py
@@ -2,11 +2,12 @@ import requests
 import os
 
 cwd = os.getcwd()
-image = cwd + "/assets/images/weather_icons/raining_test.png"
+image_name = "raspilogo.bmp"
+image = cwd + f"/assets/images/test/{image_name}"
+test_url = "http://192.168.0.213/test_image"
 
 with open(image, "rb") as f:
     img_data = f.read()
 
-files = {"image": ("raining_test.png", img_data)}
-test_url = "http://192.168.0.213/test_image"
+files = {"image": (image_name, img_data)}
 r = requests.post(test_url, files=files)
